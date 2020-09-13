2ebacf46160a9b98a237459629255971877d71b0
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-09-13 16:09:55 +0100

    Set up autofac on the service and data project

---
+++ ClassLibrary1/Autofac/MimirDataModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Mimir.Data.Context;
+using Mimir.Data.Factorys;
+using System.Linq;
+
+namespace Mimir.Data.Autofac
+{
+    public class MimirDataModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .WithParameter("context", new MirmirContext())
+                .WithParameter("retryPolicy", PollyFactory.CreateAsyncRetryPolicy())
+                .AsImplementedInterfaces();
+        }
+    }
+}
---
+++ Mimir.Service/Autofac/MimirServiceModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Mimir.Data.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimir.Service.Autofac
+{
+    public class MimirServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new MimirDataModule());
+
+            //"ThisAssembly" means "any types in the same assembly as the module"
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                 .Where(t => t.Name.EndsWith("Service"))
+                 .AsImplementedInterfaces();
+        }
+    }
+}
