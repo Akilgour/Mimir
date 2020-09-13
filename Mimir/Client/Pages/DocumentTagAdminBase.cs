63a1ee0959a14868eb0fd5791da4cf53000ebbca
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-09-13 18:30:03 +0100

    Stubbed out a document tag screen,

---
+++ Mimir/Client/Pages/DocumentTagAdminBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Mimir.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+
+namespace Mimir.Client.Pages
+{
+    public class DocumentTagAdminBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        public List<DocumentTagDisplay> DocumentTags { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            DocumentTags = await Http.GetJsonAsync<List<DocumentTagDisplay>>("api/DocumentTag");
+        }
+    }
+}
