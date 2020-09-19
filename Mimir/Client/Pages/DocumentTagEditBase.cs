a1b509bdbb85b30032db3b99b50e4cbd13c64ec8
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-09-19 20:25:02 +0100

    Stubbed out edit page

---
+++ Mimir/Client/Pages/DocumentTagEditBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Mimir.Client.Pages
+{
+    public class DocumentTagEditBase : ComponentBase
+    {
+        [Parameter]
+        public string DocumentTagId { get; set; }
+
+        protected override void OnInitialized()
+        {
+        }
+    }
+}
